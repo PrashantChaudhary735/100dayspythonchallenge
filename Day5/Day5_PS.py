389d1ca737e2638321112c0af00239b33c83649e
PrashantChaudhary735 <chaudharyprashant735@gmail.com>
2025-01-05 05:14:35 +0530

    For loop in python

--- Day5/Day5_PS.py
+++ Day5/Day5_PS.py
@@ -1,2 +1,21 @@
-Name = 'My name is Prashant B Chaudhary';
-print(Name)
+# For loop in python
+# fruits = ['apple','banana', 'pear', 'peach']
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + 'pie')
+# print(fruits)
+
+student_scores = [130, 135,234,222, 111, 135, 343, 23, 678, 383]
+# total_exam_score = sum(student_scores)
+# print(total_exam_score)
+# sum1 = 0
+# Printing the maximum element present in the list using for loop
+# print(student_scores[0])
+# max = student_scores[0]
+# for score in student_scores:
+#     if score > max:
+#         max = score
+# print(max)
+# for number in range(0, 100, 2):
+#     print(number)
+
